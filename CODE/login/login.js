3db9a153679267767bc8218fc8d5d64e8d51e2fb
Kripesh Sunuwar <kripesh.sunuwar477@gmail.com>
2023-04-27 08:21:47 +0545

    login page included

--- CODE/login/login.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#show-login").addEventListener("Click",function(){
-    document.querySelector(".popup").classList.add("active");
-});
-document.querySelector(".popup .close-btn").addEventListener("Click",function(){
-    document.querySelector(".popup").classList.remover("active");
-});
